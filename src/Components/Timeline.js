15ccf5f6d9e737cf9b64a5e7d0f78abd581b7deb
Katie Brouwers <katiebrouwers@Katies-MacBook-Pro.local>
2019-10-14 14:17:29 -0400

    new sections

--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Timeline extends React.Component {
     render() {
@@ -8,24 +9,46 @@ class Timeline extends React.Component {
                     <h4>since 1992</h4>
                     <h3><span>making</span> history</h3>
                 </div>
-                <div className="middle col-7">
-                    <div className=" col">
-                        <h3>1988</h3>
-                        <h4>Establishment</h4>
-                        <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
-                    </div>
-                    <div className=" col">
-                        <h3>1988</h3>
-                        <h4>Establishment</h4>
-                        <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
-                    </div>
-                    <div className=" col">
-                        <h3>1988</h3>
-                        <h4>Establishment</h4>
-                        <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
-                    </div>
-                </div>
-
+                <Carousel className="middle col-7">
+                    <Carousel.Item className="slide">
+                        <div className="wrapper">
+                            <span className="item">
+                                <h3>oct 2019</h3>
+                                <h4>Establishment</h4>
+                                <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
+                            </span>
+                            <span className="item mid">
+                                <h3>sept 2019</h3>
+                                <h4>Establishment</h4>
+                                <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
+                            </span>
+                            <span className="item">
+                                <h3>oct 2019</h3>
+                                <h4>Establishment</h4>
+                                <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
+                            </span>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item className="slide">
+                        <div className="wrapper">
+                            <span className="item">
+                                <h3>nov 2019</h3>
+                                <h4>Establishment</h4>
+                                <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
+                            </span>
+                            <span className="item mid">
+                                <h3>dec 2019</h3>
+                                <h4>Establishment</h4>
+                                <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
+                            </span>
+                            <span className="item">
+                                <h3>jan 2020</h3>
+                                <h4>Establishment</h4>
+                                <p>Suspe ndisse suscipit sagittis leo sit met condimentum essum dolor sit amet, consecte.</p>
+                            </span>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
             </div>
         )
     }
